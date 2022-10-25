1617e028779883d1506c9c19fd2fdf831fca8fb6
minhtaile2712 <minhtaile2712@gmail.com>
2022-10-26 01:59:32 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import YouTube from "react-youtube";
+import { useState } from "react";
+
+const videoId = "E7UlemBSoYw";
+const nodes = [
+  {
+    start: "1:00",
+    text: `my heart will go on 1
+  my heart will go on 2
+  `,
+  },
+  { start: "2:30", text: "my heart will go on 2" },
+];
+const conv = (s) => s.split(":").reduce((a, i) => 60 * a + +i, 0);
 
 function App() {
+  const opts = {
+    height: "390",
+    width: "640",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      // controls: 0,
+    },
+  };
+
+  const [player, setPlayer] = useState();
+
+  function _onReady(event) {
+    // access to player in all event handlers via event.target
+    setPlayer(event.target);
+    // event.target.pauseVideo();
+    // event.target.playVideo();
+  }
+
+  function playVideo() {
+    player.playVideo();
+  }
+
+  function pauseVideo() {
+    player.pauseVideo();
+  }
+
+  // function seekTo(seconds) {
+  //   player.seekTo(seconds);
+  // }
+
+  const [fontSize, setFontSize] = useState(16);
+
+  function seek(e) {
+    player.seekTo(conv(e.currentTarget.dataset.start));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <YouTube videoId={videoId} opts={opts} onReady={_onReady} />
+      <button onClick={playVideo}>Play</button>
+      <button onClick={pauseVideo}>Pause</button>
+      <span>Font Size:</span>
+      <input
+        type="number"
+        onChange={(e) => setFontSize(e.target.value)}
+        value={fontSize}
+      ></input>
+      <div>
+        {nodes.map((node) => (
+          <div data-start={node.start} onClick={seek} className="Paragraph">
+            <div className="Start">{node.start}</div>
+            <div className="Text" style={{ fontSize: `${fontSize}px` }}>
+              {node.text}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
